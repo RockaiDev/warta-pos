b95841984a78c4c61e4137d719c13cc4e9242a80
Rockai Dev Company <113069468+RockaiDev@users.noreply.github.com>
2025-05-10 14:53:00 +0300

    Fix SWC binary loading error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false // Disable SWC minification temporarily
+  swcMinify: false, // Disable SWC minification temporarily
+  experimental: {
+    forceSwcTransforms: true // Force using SWC for transforms
+  }
 };
 
 export default nextConfig;
